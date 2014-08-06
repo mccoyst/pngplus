158f177ad074090463da380a082417fe7705631e
Steve McCoy <mccoyst@gmail.com>
2014-08-06 14:35:11 -0400

    Make iTXt like the spec. Fixes #1.

--- itxt.go
+++ itxt.go
@@ -4,6 +4,7 @@ package pngplus
 
 import (
 	"bytes"
+	"compress/zlib"
 	"encoding/binary"
 	"hash/crc32"
 	"io"
@@ -11,7 +12,7 @@ import (
 
 // EncodeITXT writes to w an iTXt chunk with data from s.
 // If len(s) cannot fit in a uint32, it is truncated.
-func EncodeITXT(w io.Writer, s string) error {
+func EncodeITXT(w io.Writer, compress bool, key, lang, s string) error {
 	var b bytes.Buffer
 	err := binary.Write(&b, binary.BigEndian, uint32(len(s)))
 	if err != nil {
@@ -19,7 +20,31 @@ func EncodeITXT(w io.Writer, s string) error {
 	}
 
 	b.WriteString("iTXt")
-	b.WriteString(s)
+	b.WriteString(key)
+	b.WriteByte(0)
+
+	if compress {
+		b.WriteByte(1)
+	} else {
+		b.WriteByte(0)
+	}
+
+	b.WriteByte(0)
+	b.WriteString(lang)
+	b.WriteByte(0)
+	// forget about translated key for now
+	b.WriteByte(0)
+
+	if !compress {
+		b.WriteString(s)
+	} else {
+		z := zlib.NewWriter(&b)
+		_, err = z.Write([]byte(s))
+		z.Close()
+		if err != nil {
+			return err
+		}
+	}
 
 	buf := b.Bytes()
 	err = binary.Write(&b, binary.BigEndian, crc32.ChecksumIEEE(buf[4:]))
--- itxt_test.go
+++ itxt_test.go
@@ -11,17 +11,16 @@ import (
 
 func TestEncodeITXT(t *testing.T) {
 	var w bytes.Buffer
+	key := "blah"
+	lang := "en-US"
 	s := "hello"
 
-	err := EncodeITXT(&w, s)
+	err := EncodeITXT(&w, false, key, lang, s)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
 	bs := w.Bytes()
-	if len(bs) != 4*3 + len(s) {
-		t.Fatalf("wrong length for output: %x", bs)
-	}
 
 	var n uint32
 	err = binary.Read(bytes.NewBuffer(bs), binary.BigEndian, &n)
@@ -33,23 +32,61 @@ func TestEncodeITXT(t *testing.T) {
 		t.Fatalf("wrong chunk length: %d vs. %d", n, len(s))
 	}
 
-	g := string(bs[4:8])
-	if g != "iTXt" {
-		t.Fatalf("wrong chunk type: %q", g)
-	}
-
-	g = string(bs[8:8+len(s)])
-	if g != s {
-		t.Fatalf("wrong chunk content: %q vs. %q", g, s)
+	content := string(bs[4:len(bs)-4])
+	expected := "iTXtblah\x00\x00\x00en-US\x00\x00hello"
+	if content != expected {
+		t.Fatalf("wrong content: %q vs. %q", content, expected)
 	}
 
 	var crc uint32
-	err = binary.Read(bytes.NewBuffer(bs[8+len(s):]), binary.BigEndian, &crc)
+	err = binary.Read(bytes.NewBuffer(bs[len(bs)-4:]), binary.BigEndian, &crc)
 	if err != nil {
 		t.Fatalf("unexpected crc read error: %v", err)
 	}
 
-	if crc != crc32.ChecksumIEEE([]byte("iTXthello")) {
-		t.Fatalf("wrong crc: %d vs. %d", crc, 666)
+	excrc := crc32.ChecksumIEEE([]byte(expected))
+	if crc != excrc {
+		t.Fatalf("wrong crc: %d vs. %d", crc, excrc)
+	}
+}
+
+func TestEncodeITXTCompressed(t *testing.T) {
+	var w bytes.Buffer
+	key := "blah"
+	lang := "en-US"
+	s := "hello"
+
+	err := EncodeITXT(&w, true, key, lang, s)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	bs := w.Bytes()
+
+	var n uint32
+	err = binary.Read(bytes.NewBuffer(bs), binary.BigEndian, &n)
+	if err != nil {
+		t.Fatalf("unexpected length read error: %v", err)
+	}
+
+	if n != uint32(len(s)) {
+		t.Fatalf("wrong chunk length: %d vs. %d", n, len(s))
+	}
+
+	content := string(bs[4:len(bs)-4])
+	expected := "iTXtblah\x00\x01\x00en-US\x00\x00\x78\x9c\xca\x48\xcd\xc9\xc9\x07\x04\x00\x00\xff\xff\x06\x2c\x02\x15"
+	if content != expected {
+		t.Fatalf("wrong content:\n%x\nvs.\n%x", content, expected)
+	}
+
+	var crc uint32
+	err = binary.Read(bytes.NewBuffer(bs[len(bs)-4:]), binary.BigEndian, &crc)
+	if err != nil {
+		t.Fatalf("unexpected crc read error: %v", err)
+	}
+
+	excrc := crc32.ChecksumIEEE([]byte(expected))
+	if crc != excrc {
+		t.Fatalf("wrong crc: %d vs. %d", crc, excrc)
 	}
 }
